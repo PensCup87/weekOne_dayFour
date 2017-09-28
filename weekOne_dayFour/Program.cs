f46bfeeb9ec1817fb41cbd33fbe5f9fb57f8f642
PensCup87 <bdt9123@gmail.com>
2017-09-28 16:10:32 -0400

    More Practice problems

--- weekOne_dayFour/Program.cs
+++ weekOne_dayFour/Program.cs
@@ -15,18 +15,64 @@ namespace weekOne_dayFour
             // - Contents are in DOUBLE QUOTES
 
 
-            Console.WriteLine("This is a tab \t.");
-            Console.WriteLine("This is a \"quote\".");
-            Console.WriteLine("This is a backslash\\");
-            Console.WriteLine("This is a \n new line.");
-            Console.WriteLine("This is a bell \a");
+            //Console.WriteLine("This is a tab \t.");
+            //Console.WriteLine("This is a \"quote\".");
+            //Console.WriteLine("This is a backslash\\");
+            //Console.WriteLine("This is a \n new line.");
+            //Console.WriteLine("This is a bell \a");
 
-            string firstName = "Daniel";
-            string dogName = "Gus";
+            //string firstName = "Daniel";
+            //string dogName = "Gus";
 
-            Console.WriteLine("His name is {1} and his dog is {1}.", firstName, dogName);
+            //Console.WriteLine("His name is {1} and his dog is {1}.", firstName, dogName);
 
+            //Console.WriteLine(firstName.Length);
+
+            //End Of Class Practice Problems:
+
+            string firstName = "Brian";
+            int firstNameLength = firstName.Length;
             Console.WriteLine(firstName.Length);
+            string lastName = "Tome";
+            int lastNameLength = lastName.Length;
+            Console.WriteLine(lastName.Length);
+
+            if (firstNameLength > lastNameLength)
+            {
+                Console.WriteLine("First is longer");
+            }
+            else if (firstNameLength == lastNameLength)
+            {
+                Console.WriteLine("Samsis");
+            }
+            else
+            {
+                Console.WriteLine("Last must be longer");
+            }
+
+            Console.WriteLine("This is tab /t & this is a \"quote\" & this is a backslash\\ \n && \a");
+
+            string dudeName = "Bill";
+            string nickName = "Pedro";
+            
+            //if (dudeName == nickName)
+            //{
+            //    Console.WriteLine("The names are the same.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("The names are different");
+            //}
+
+            if (dudeName.Equals(nickName))
+                {
+                Console.WriteLine("The names are the same");
+            }
+            else
+            {
+                Console.WriteLine("The names are not equal");
+            }
+            
 
 
 

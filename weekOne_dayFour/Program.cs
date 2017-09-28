c81ec160ddebb7826d0f25ce7c28f65bc7a785a6
PensCup87 <bdt9123@gmail.com>
2017-09-28 14:37:23 -0400

    concatination example

--- weekOne_dayFour/Program.cs
+++ weekOne_dayFour/Program.cs
@@ -10,6 +10,32 @@ namespace weekOne_dayFour
     {
         static void Main(string[] args)
         {
+            //STRINGS
+            // - consist of various characters, symbols, or numbers (a number can not be used for MATH)
+            // - Contents are in DOUBLE QUOTES
+
+
+            Console.WriteLine("This is a tab \t.");
+            Console.WriteLine("This is a \"quote\".");
+            Console.WriteLine("This is a backslash\\");
+            Console.WriteLine("This is a \n new line.");
+            Console.WriteLine("This is a bell \a");
+
+            string firstName = "Daniel";
+            string dogName = "Gus";
+
+            Console.WriteLine("His name is {1} and his dog is {1}.", firstName, dogName);
+
+            Console.WriteLine(firstName.Length);
+
+
+
+
+
+
+
+
+
         }
     }
 }
